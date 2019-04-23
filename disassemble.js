2c71b08547c0660ab0c6c103411d4a918d996acb
MMichael42 <michaeld.johnsoncst@gmail.com>
2019-04-23 16:55:21 -0700

    fixed issue where we were getting the wrong registerY four bit identifier (we weren't doing a zero fill right shift to get the extra bits to fall off)

--- disassemble.js
+++ disassemble.js
@@ -3,6 +3,12 @@ console.log('CHIP-8 Disassembler');
 let fishie = '../roms/Fishie.ch8';
 let pong = '../roms/pong.ch8';
 
+let bytetest = 0xef;
+let masked = (bytetest & 0xf0) >>> 4;
+console.log(bytetest.toString(2));
+console.log(masked.toString(2));
+
+
 // load the file
 fetch(fishie)
   .then( response => {
@@ -74,7 +80,8 @@ function decodeRom(romBuffer, programCounter) {
   let AddressAndHexInfo = (addressSpace + ': $' + byte1HexString + byte2HexString + '\n').toUpperCase();
   let addressHi = (byte1 & 0x0F).toString(16); // high 4 bits of 12bit address
   let registerX = (byte1 & 0x0F).toString(16); // registerX, lower 4 bits of the first byte
-  let registerY = (byte2 % 0xF0).toString(16) // registerY, high 4 bits from second byte
+  let registerY = ((byte2 % 0xF0) >>> 4).toString(16) // registerY, high 4 bits from second byte
+
   let decodedString = '';
 
   // opcodes
@@ -119,15 +126,64 @@ function decodeRom(romBuffer, programCounter) {
       decodedString = `SE V${registerX}, V${registerY}`;
       break;
     case 0x06:
-      // 6XNN: Sets Vx to NN
+      // 6xnn 
+      // Sets Vx to NN
       decodedString = `MVI V${registerX}, #$${byte2HexString}`.toUpperCase();
       break;
     case 0x07:
-      // 7xkk: adds kk to Vx, then stores that in Vx
+      // 7xkk
+      // adds kk to Vx, then stores that in Vx
       decodedString =  `ADD V${registerX}, #$${byte2HexString}`;
       break;
-    case 0x08:
+    case 0x08:      
       decodedString = '0x08 not handled yet';
+      switch(byte2 & 0x0F) {
+        case 0x00:
+          // 8xy0
+          // set Vx = Vy
+          decodedString = `LD V${registerX}, V${registerY}`;
+          break;
+        case 0x01:
+          // 8xy1
+          // set Vx = Vx OR Vx
+          decodedString = `OR V${registerX}, V${registerY}`;
+          break;
+        case 0x02:
+          // 8xy2
+          // set Vx = Vx AND Vy
+          decodedString = `AND V${registerX}, V${registerY}`;
+          break;
+        case 0x03:
+          // 8xy3
+          // set Vx = Vx OR Vy
+          decodedString = `XOR V${registerX}, V${registerY}`;
+          break;
+        case 0x04:
+          // 8xy4
+          // set Vx = Vx + Vy, set VF = carry
+          decodedString = `ADD V${registerX}, V${registerY}`;
+          break;
+        case 0x05:
+          // 8xy5
+          // set Vx = Vx - Vy, set VF = NOT borrow
+          decodedString = `SUB V${registerX}, V${registerY}`;
+          break;
+        case 0x06:
+          // 8xy6
+          // set Vx = Vx SHR 1
+          decodedString = `SHR V${registerX} {, V${registerY}}`;
+          break;
+        case 0x07:
+          // 8xy7
+          // set Vx = Vy - Vx, set VF = NOT borrow
+          decodedString = `SUBN V${registerX}, V${registerY}`;
+          break;
+        case 0x0E:
+          // 8xyE
+          // set Vx = Vx SHL 1
+          decodedString = `SHL V${registerX} {, V${registerY}}`;
+          break;
+      }
       break;
     case 0x09:
       decodedString = '0x09 not handled yet';
@@ -156,9 +212,5 @@ function decodeRom(romBuffer, programCounter) {
       decodedString = 'unrecongized opcode - something went wrong';
       break;
   }
-  return AddressAndHexInfo + decodedString;
+  return (AddressAndHexInfo + decodedString).toUpperCase();
 }
-
-// function decodeRom(romBuffer, programCounter) {
-//   console.log(decodeOpcode(romBuffer[programCounter], romBuffer[programCounter + 1]));
-// }
